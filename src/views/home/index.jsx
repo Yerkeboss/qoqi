1c1ebe199053c7693ada4a1becb229168c671100
Yerke <zh.yerkegaly@gmail.com>
2024-04-04 11:17:11 +0500

    products-buttons-filter

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -13,7 +13,7 @@ import {
   useScrollTop,
 } from "@/hooks";
 import bannerImg from "@/images/banner-girl.png";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { shallowEqual, useSelector } from "react-redux";
 import { selectFilter } from "@/selectors/selectorEvent";
@@ -26,10 +26,20 @@ import {
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Button from "react-bootstrap/Button";
+import { useDispatch } from "react-redux";
+import { applyFilter } from "../../redux/actions/filterActions";
 
 const Home = () => {
   useDocumentTitle("Qoqiqaz | Home");
   useScrollTop();
+  const dispatch = useDispatch();
+  const [selectedBrand, setSelectedBrand] = useState("");
+
+  const handleBrandSelect = (brand) => {
+    setSelectedBrand(brand);
+    dispatch(applyFilter({ brand }));
+  };
 
   const store = useSelector(
     (state) => ({
@@ -90,6 +100,25 @@ const Home = () => {
         </EventList>
 
         <h2>Галерея лучших работ</h2>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <Button onClick={() => handleBrandSelect("")}>Все категории</Button>
+          <Button onClick={() => handleBrandSelect("фотографии")}>
+            Фотографии
+          </Button>
+          <Button onClick={() => handleBrandSelect("музыка")}>Музыка</Button>
+          <Button onClick={() => handleBrandSelect("дизайн")}>Дизайн</Button>
+          <Button onClick={() => handleBrandSelect("иллюстрации")}>
+            Иллюстрации
+          </Button>
+          <Button onClick={() => handleBrandSelect("анимации")}>
+            Анимации
+          </Button>
+          <Button onClick={() => handleBrandSelect("инсталяции")}>
+            Инсталяции
+          </Button>
+          <Button onClick={() => handleBrandSelect("3D")}>3D</Button>
+        </div>
+        <br />
         <AppliedFilters filteredProductsCount={store.filteredProducts.length} />
         <ProductList {...store}>
           <ProductGrid products={store.filteredProducts} />
