77fd9d97a951756786f117f5c33bff47509f8650
Yerke <zh.yerkegaly@gmail.com>
2024-04-14 23:01:09 +0500

    fix-deploy

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,26 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+			include: /\.(jsx|tsx)$/,
+			babel: {
+				plugins: ['styled-components'],
+				babelrc: false,
+				configFile: false,
+			},
+		}),
   ],
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
